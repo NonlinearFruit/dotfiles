10bfb8bb613e837079be94fc18fa96eb6a5d28c9
NonlinearFruit <1123benji5813@gmail.com>
2021-06-19 16:36:02 -0500

    Add newsboat for rss feeds

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,6 +3,7 @@ pkg.install() {
   mkdir ~/.vim/swapfiles
   mkdir ~/.vim/undofiles
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  mkdir ~/.newsboat
   mkdir ~/scripts
   curl https://raw.githubusercontent.com/dwyl/english-words/master/words.txt | grep -e "^[a-z]\+$" > $ELLIPSIS_HOME/scripts/words.txt
 }
@@ -17,6 +18,15 @@ pkg.link() {
     if [ -f "$file" ]; then
       filename=$(basename "$file")
       fs.link_file $file "$ELLIPSIS_HOME/scripts/$filename"
+    fi
+  done
+
+  # Add newsboat links
+  mkdir -p "$ELLIPSIS_HOME/.newsboat"
+  for file in $PKG_PATH/.newsboat/*; do
+    if [ -f "$file" ]; then
+      filename=$(basename "$file")
+      fs.link_file $file "$ELLIPSIS_HOME/.newsboat/$filename"
     fi
   done
 }
