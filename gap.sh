36e7ae5565d573ca52cb3854ba0105ef7297c443
NonlinearFruit <1123benji5813@gmail.com>
2022-12-30 12:24:49 -0600

    Hard link and cp if linking fails

--- gap.sh
+++ gap.sh
@@ -2,7 +2,7 @@
 
 mapping_to_apply="mappings/$1.json"
 default_mapping="mappings/common.json"
-json_to_symlink='.[] | "ln -f \(.[0]) \(.[1])"'
+json_to_symlink='.[] | "ln -f \(.[0]) \(.[1]) 2> /dev/null || cp \(.[0]) \(.[1])"'
 
 if [ -f "$default_mapping" ]; then
   jq "$json_to_symlink" --raw-output $default_mapping
