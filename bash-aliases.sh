029b968ec1fe4a96856e8e8a1fe2a461b19541b4
NonlinearFruit <1123benji5813@gmail.com>
2023-03-10 14:44:43 -0700

    Termux copy/paste

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -12,3 +12,8 @@ alias tmux-clean='tmux ls -F "#{session_attached} #{session_group} #{session_id}
 alias tmux-join='tmux ls 2> /dev/null > /dev/null && tmux new -t $(tmux ls -F "#{session_id}" | sed "s/\$//" | head -1)\; new-window -c "$(pwd)" || tmux'
 alias vim="$EDITOR"
 alias youtube-dl-audio='youtube-dl --ignore-errors --output "%(title)s.%(ext)s" --extract-audio --audio-format mp3'
+
+if is_termux ; then
+ alias copy='termux-clipboard-set'
+ alias paste='termux-clipboard-get'
+fi
