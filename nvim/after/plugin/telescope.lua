242bf68ea3bc3155e7d9c32684a5bfed21f1ca51
NonlinearFruit <1123benji5813@gmail.com>
2023-03-22 23:46:03 -0500

    Finish mapping desireable telescope builtins

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -10,47 +10,37 @@ end
 telescope.load_extension('fzf')
 
 local prefix = "<leader>/"
+vim.keymap.set("n", prefix.."/", require('telescope.builtin').current_buffer_fuzzy_find, { desc = "[/] Search Fuzzy Find [/] (search search)" })
+vim.keymap.set("n", prefix.."?", require('telescope.builtin').man_pages, { desc = "[/] Search [M]an Pages" })
 vim.keymap.set("n", prefix.."a", require('telescope.builtin').builtin, { desc = "[/] Search [A]ll" })
 vim.keymap.set("n", prefix.."b", require('telescope.builtin').buffers, { desc = "[/] Search [B]uffers" })
 vim.keymap.set("n", prefix.."c", require('telescope.builtin').commands, { desc = "[/] Search [C]ommands" })
-vim.keymap.set("n", prefix.."f", require('telescope.builtin').current_buffer_fuzzy_find, { desc = "[/] Search [F]uzzy Find" })
+vim.keymap.set("n", prefix.."f", require('telescope.builtin').find_files, { desc = "[/] Search [F]iles (all in working directory)" })
+vim.keymap.set("n", prefix.."g", require('telescope.builtin').live_grep, { desc = "[/] Search with [G]rep" })
 vim.keymap.set("n", prefix.."h", require('telescope.builtin').help_tags, { desc = "[/] Search [H]elp" })
 vim.keymap.set("n", prefix.."k", require('telescope.builtin').keymaps, { desc = "[/] Search [K]eymaps" })
+vim.keymap.set("n", prefix.."l", require('telescope.builtin').git_files, {desc = "[/] Search Git Files [L] (ls git stuff?)" })
+vim.keymap.set("n", prefix.."m", require('telescope.builtin').marks, { desc = "[/] Search [M]arks" })
 vim.keymap.set("n", prefix.."r", require('telescope.builtin').registers, { desc = "[/] Search [R]egisters" })
 vim.keymap.set("n", prefix.."s", require('telescope.builtin').spell_suggest, { desc = "[/] Search [S]pell" })
-
--- Telescope commands to map
-  -- git_branches
-  -- git_files
-  -- live_grep
-  -- man_pages
-  -- marks
+vim.keymap.set("n", prefix.."y", require('telescope.builtin').git_branches, { desc = "[/] Search Git Branches [Y] (looks like branching)" })
 
 -- Telescope builtins
   -- autocommands
-  -- buffers
-  -- builtin
   -- colorscheme
   -- command_history
-  -- commands
   -- current_buffer_fuzzy_find
   -- current_buffer_tags
   -- diagnostics
   -- fd
   -- filetypes
-  -- find_files
   -- git_bcommits
-  -- git_branches
   -- git_commits
-  -- git_files
   -- git_stash
   -- git_status
   -- grep_string
-  -- help_tags
   -- highlights
   -- jumplist
-  -- keymaps
-  -- live_grep
   -- loclist
   -- lsp_definitions
   -- lsp_document_symbols
@@ -61,18 +51,14 @@ vim.keymap.set("n", prefix.."s", require('telescope.builtin').spell_suggest, { d
   -- lsp_references
   -- lsp_type_definitions
   -- lsp_workspace_symbols
-  -- man_pages
-  -- marks
   -- oldfiles
   -- pickers
   -- planets
   -- quickfix
   -- quickfixhistory
-  -- registers
   -- reloader
   -- resume
   -- search_history
-  -- spell_suggest
   -- symbols
   -- tags
   -- tagstack
