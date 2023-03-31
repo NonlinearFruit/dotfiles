357654a630fc2bdd54ff471a323421d3b1dd69be
NonlinearFruit <1123benji5813@gmail.com>
2023-03-31 16:26:04 -0500

    Separate the wsl aliases + vi + npp

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-alias copy="clip.exe"
-alias dotnet='dotnet.exe'
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
-alias paste="powershell.exe Get-Clipboard"
-alias podman="podman.exe"
 alias populars='history | sed -E "s/^[ ]+[0-9]+[ ]+//" | cut -d" " -f1 | sort | uniq -c --repeated | sort -r'
-alias rider='rider64.exe'
 alias tmux-clean='tmux ls -F "#{session_attached} #{session_group} #{session_id}" | grep ^0 | sed "s/^0 //" | sed "s/ [$]/-/" | xargs -I % tmux kill-session -t %'
 alias tmux-join='tmux ls 2> /dev/null > /dev/null && tmux new -t $(tmux ls -F "#{session_id}" | sed "s/\$//" | head -1)\; new-window -c "$(pwd)" || tmux'
+alias vi="$EDITOR"
 alias vim="$EDITOR"
 alias youtube-dl-audio='youtube-dl --ignore-errors --output "%(title)s.%(ext)s" --extract-audio --audio-format mp3'
 
 if is_termux ; then
- alias copy='termux-clipboard-set'
- alias paste='termux-clipboard-get'
+  alias copy='termux-clipboard-set'
+  alias paste='termux-clipboard-get'
+fi
+
+if is_wsl ; then
+  alias copy="clip.exe"
+  alias dotnet='dotnet.exe'
+  alias npp='notepad++.exe'
+  alias paste="powershell.exe Get-Clipboard"
+  alias podman="podman.exe"
 fi
