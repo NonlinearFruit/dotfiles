cc4f63c0d1b29cd7e04c33407bdb41c8785719ef
NonlinearFruit <1123benji5813@gmail.com>
2023-04-13 13:23:09 -0500

    Inline code highlighting for markdown

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -4,7 +4,7 @@ end
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "c_sharp", "lua", "vim", "python", "jq", "rust", "yaml", "toml", "markdown", "latex", "json", "javascript", "bash" },
+  ensure_installed = { "c_sharp", "lua", "vim", "python", "jq", "rust", "yaml", "toml", "markdown", "markdown_inline", "latex", "json", "javascript", "bash" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
