6765777848f463891df6349610dd04eec4ee4ef5
NonlinearFruit <1123benji5813@gmail.com>
2023-04-19 16:46:37 -0500

    Dockerfile lsp + LSP keymap notes

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -23,6 +23,8 @@ mason_lsp_config.setup({
   automatic_installation = true
 })
 
+-- vim.lsp.set_log_level('debug')
+
 -- How diagnostics show up
 vim.diagnostic.config({
   virtual_text = false,
@@ -37,13 +39,13 @@ local function on_attach(client, bufnr)
   vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, {buffer = bufnr, remap = false, desc = "[G]oto [R]eferences"})
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, {buffer = bufnr, remap = false, desc = "[K]now what this is"})
   vim.keymap.set({"n", "i", "v"}, "<c-k>", function() vim.diagnostic.open_float() end, {buffer = bufnr, remap = false, desc = "[K]now what is wrong"})
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, {buffer = bufnr, remap = false, desc = "[[] Next [d]iagnostic"})
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, {buffer = bufnr, remap = false, desc = "[]] Previous [d]iagnostic"})
+  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, {buffer = bufnr, remap = false, desc = "[C]ode [A]ctions"})
 
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, {buffer = bufnr, remap = false})
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, {buffer = bufnr, remap = false})
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, {buffer = bufnr, remap = false})
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, {buffer = bufnr, remap = false})
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, {buffer = bufnr, remap = false})
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, {buffer = bufnr, remap = false})
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, {buffer = bufnr, remap = false})
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, {buffer = bufnr, remap = false})
 end
 
 -- Unused vim.lsp.buf.*
@@ -101,6 +103,10 @@ lsp_config.rust_analyzer.setup({
 })
 
 lsp_config.bashls.setup({
+  on_attach = on_attach
+})
+
+lsp_config.dockerls.setup({
   on_attach = on_attach
 })
 
