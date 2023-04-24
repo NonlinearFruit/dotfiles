19927acb7ec27fda370467ab28fdeac7d1e76027
NonlinearFruit <1123benji5813@gmail.com>
2023-04-23 23:57:09 -0500

    Automate more tool setup

---
+++ setups/tools.sh
@@ -0,0 +1,42 @@
+# autojump: changing directory (requires .bashrc sourcing)
+# dos2unix: converts files from dos and unix and back again
+# figlet: ascii font
+# tmux: Terminal multiplexing
+# jq: json query
+# curl: http requests
+sudo apt install -y autojump curl dos2unix figlet jq tmux
+
+# bat: better cat https://github.com/sharkdp/bat
+cargo install bat
+
+# bob: nvim version manager https://github.com/MordechaiHadad/bob
+cargo install bob-nvim
+
+# fd: better find https://github.com/sharkdp/fd
+cargo install fd-find
+
+# fnm: npde versioning https://github.com/Schniz/fnm
+cargo install fnm
+
+# htmlq: jq for html using css selectors https://github.com/mgdm/htmlq
+cargo install htmlq
+
+# ripgrep: better grep https://github.com/BurntSushi/ripgrep
+cargo install ripgrep
+
+# tldr: examples of using different commands https://github.com/dbrgn/tealdeer
+cargo install tealdeer
+
+# z: better autojump https://github.com/ajeetdsouza/zoxide
+cargo install zoxide
+
+# speedtest-cli: collect up/down network data
+# pip install speedtest-cli
+# PATH="${PATH}:${HOME}/.local/bin"
+# /home/pi/.local/bin/speedtest-cli --csv-header > /home/pi/xfinity_speed.csv
+# crontab -e
+# */10 * * * * /home/pi/.local/bin/speedtest-cli --csv >> /home/pi/xfinity_speed.csv
+
+# wolfram engine + wolfram script
+## https://www.wolfram.com/wolframscript/?source=nav
+## activation key how to: https://mathematica.stackexchange.com/a/215202
