c8fcd8156ac4f22250b4dbff3a6b772f696f3621
NonlinearFruit <1123benji5813@gmail.com>
2023-04-24 19:39:48 -0500

    Add work around for omnisharp/roslyn bug

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -111,7 +111,20 @@ lsp_config.dockerls.setup({
 })
 
 lsp_config.omnisharp.setup({
-  on_attach = on_attach,
+  on_attach = function (client, bufnr)
+    -- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483#issuecomment-1492605642
+    local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+    for i, v in ipairs(tokenModifiers) do
+      local tmp = string.gsub(v, ' ', '_')
+      tokenModifiers[i] = string.gsub(tmp, '-_', '')
+    end
+    local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+    for i, v in ipairs(tokenTypes) do
+      local tmp = string.gsub(v, ' ', '_')
+      tokenTypes[i] = string.gsub(tmp, '-_', '')
+    end
+    on_attach(client, bufnr)
+  end,
   --capabilities = capabilities,
   root_dir = lsp_config.util.root_pattern(".sln", ".csproj", ".git"),
 })
