12fe1f13c3819d4827dcd9af531e084c869ff2ac
NonlinearFruit <1123benji5813@gmail.com>
2023-05-09 23:48:41 -0500

    More consistent descriptions

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -34,18 +34,18 @@ vim.diagnostic.config({
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local function on_attach(client, bufnr)
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, {buffer = bufnr, remap = false, desc = "[G]oto [D]efinition"})
-  vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, {buffer = bufnr, remap = false, desc = "[G]oto [D]efinition"})
-  vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, {buffer = bufnr, remap = false, desc = "[G]oto [R]eferences"})
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, {buffer = bufnr, remap = false, desc = "[g]oto [d]efinition"})
+  vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, {buffer = bufnr, remap = false, desc = "[g]oto [d]efinition"})
+  vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, {buffer = bufnr, remap = false, desc = "[g]oto [r]eferences"})
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, {buffer = bufnr, remap = false, desc = "[K]now what this is"})
-  vim.keymap.set({"n", "i", "v"}, "<c-k>", function() vim.diagnostic.open_float() end, {buffer = bufnr, remap = false, desc = "[K]now what is wrong"})
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, {buffer = bufnr, remap = false, desc = "[[] Next [d]iagnostic"})
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, {buffer = bufnr, remap = false, desc = "[]] Previous [d]iagnostic"})
-  vim.keymap.set({"n", "v"}, "<leader>ca", function() vim.lsp.buf.code_action() end, {buffer = bufnr, remap = false, desc = "[C]ode [A]ctions"})
+  vim.keymap.set({"n", "i", "v"}, "<c-k>", function() vim.diagnostic.open_float() end, {buffer = bufnr, remap = false, desc = "[k]now what is wrong"})
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, {buffer = bufnr, remap = false, desc = "[]] Next [d]iagnostic"})
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, {buffer = bufnr, remap = false, desc = "[[] Previous [d]iagnostic"})
+  vim.keymap.set({"n", "v"}, "<leader>ca", function() vim.lsp.buf.code_action() end, {buffer = bufnr, remap = false, desc = "[c]ode [a]ctions"})
+  vim.keymap.set({"n", "v"}, "<leader>rr", function() vim.lsp.buf.rename() end, {buffer = bufnr, remap = false, desc = "[r]efactor [r]ename"})
 
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, {buffer = bufnr, remap = false})
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, {buffer = bufnr, remap = false})
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, {buffer = bufnr, remap = false})
 end
 
 -- Unused vim.lsp.buf.*
