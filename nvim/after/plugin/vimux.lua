226ea313f11c20954d30fb69f7cd6042586af69a
NonlinearFruit <1123benji5813@gmail.com>
2023-05-16 16:07:01 -0500

    Vertical vimux

--- nvim/after/plugin/vimux.lua
+++ nvim/after/plugin/vimux.lua
@@ -3,6 +3,8 @@ if not packer_plugins["vimux"] or not packer_plugins["vimux"].loaded then
 end
 
 vim.g.VimuxResetSequence = ""
+vim.g.VimuxOrientation = "h"
+vim.g.VimuxHeight = "40"
 vim.keymap.set("n", "<leader>vd", "<cmd>VimuxCloseRunner<cr>", { desc = "[V]imux [D]elete" })
 vim.keymap.set("n", "<leader>vp", "<cmd>VimuxPromptCommand<cr>", { desc = "[V]imux [P]rompt" })
 vim.keymap.set("n", "<leader>vz", "<cmd>VimuxZoomRunner<cr>", { desc = "[V]imux [Z]oom (tmux <leader>-z to toggle https://superuser.com/a/576505/468052)" })
