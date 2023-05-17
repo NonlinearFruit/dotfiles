3defe80d15bbd81fb1ad598d2fa3bcbb612e3b96
NonlinearFruit <1123benji5813@gmail.com>
2023-05-17 16:20:00 -0500

    Source control wezterm config

---
+++ wezterm/standard-config.lua
@@ -0,0 +1,17 @@
+local wezterm = require('wezterm')
+local action = wezterm.action
+
+local config = wezterm.config_builder()
+config.color_scheme = 'Black Metal (base16)'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = "NeverPrompt"
+config.window_padding = { top = 0, bottom = 0, left = 0, right = 0 }
+config.keys = {
+  { key = "F11", action = action.ToggleFullScreen },
+  { key = "c", mods = "CTRL|SHIFT", action = action.CopyTo("Clipboard") },
+  { key = "v", mods = "CTRL|SHIFT", action = action.PasteFrom("Clipboard") },
+  { key = "p", mods = "CTRL|SHIFT", action = action.ActivateCommandPalette },
+}
+config.font_size = 24
+config.enable_scroll_bar = false
+return config
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,3 @@
+local config = require('standard-config')
+config.default_prog = { "wsl.exe", "~"}
+return config
