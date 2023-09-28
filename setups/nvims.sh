e26c3cad050d2311828c2c596221dfd000494f83
NonlinearFruit <1123benji5813@gmail.com>
2023-09-28 13:55:16 -0500

    Automate adding preconfiged nvims

---
+++ setups/nvims.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+git clone --depth 1 https://github.com/nvim-lua/kickstart.nvim.git ~/.config/nvim-kickstart
+git clone --depth 1 https://github.com/NvChad/NvChad ~/.config/nvim-nvchad
+git clone --depth 1 https://github.com/doom-neovim/doom-nvim.git ~/.config/nvim-doom
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim-astro
+git clone --depth 1 https://github.com/LazyVim/starter ~/.config/nvim-lazy
