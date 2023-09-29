9e813facd0959951a483e67c60f474126ac656cd
NonlinearFruit <1123benji5813@gmail.com>
2023-09-28 23:44:51 -0500

    Unused alias

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -7,7 +7,6 @@ alias tmux-clean='tmux ls -F "#{session_attached} #{session_group} #{session_id}
 alias tmux-join='tmux ls 2> /dev/null > /dev/null && tmux new -t $(tmux ls -F "#{session_id}" | sed "s/\$//" | head -1)\; new-window -c "$(pwd)" || tmux'
 alias vi="$EDITOR"
 alias vim="$EDITOR"
-alias youtube-dl-audio='youtube-dl --ignore-errors --output "%(title)s.%(ext)s" --extract-audio --audio-format mp3'
 
 if is wsl ; then
   alias npp='notepad++.exe'
