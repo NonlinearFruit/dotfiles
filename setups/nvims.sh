bec259f0f43ce7809b2117a71ed229e644ec14d8
NonlinearFruit <1123benji5813@gmail.com>
2023-11-27 16:14:03 -0600

    Make vanilla vim an option

--- setups/nvims.sh
+++ setups/nvims.sh
@@ -5,3 +5,4 @@ git clone --depth 1 https://github.com/NvChad/NvChad ~/.config/nvim-nvchad
 git clone --depth 1 https://github.com/doom-neovim/doom-nvim.git ~/.config/nvim-doom
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim-astro
 git clone --depth 1 https://github.com/LazyVim/starter ~/.config/nvim-lazy
+mkdir ~/.config/nvim-vanilla
