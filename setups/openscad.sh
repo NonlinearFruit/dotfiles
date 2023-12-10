cefc992e5058021003555e9f60eb11d6e3045229
NonlinearFruit <1123benji5813@gmail.com>
2023-12-09 19:02:58 -0700

    Steps to install openscad

---
+++ setups/openscad.sh
@@ -0,0 +1,5 @@
+# OpenSCAD (https://openscad.org/)
+sudo dnf install -y openscad-MCAD
+
+# BOSL (https://github.com/revarbat/BOSL)
+git clone --depth 1 https://github.com/revarbat/BOSL.git ~/.local/share/OpenSCAD/libraries/BOSL
