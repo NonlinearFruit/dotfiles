9f842ed225baa370adefaf0cc0758e62d2a306c6
NonlinearFruit <1123benji5813@gmail.com>
2023-12-09 19:02:03 -0700

    Better nushell UX

---
+++ nvim/snippets/nu.lua
@@ -0,0 +1,36 @@
+local luasnip = require("luasnip")
+local snippet = luasnip.snippet
+local t = luasnip.text_node
+local i = luasnip.insert_node
+local s = luasnip.snippet
+local sn = luasnip.snippet_node
+local f = luasnip.function_node
+local c = luasnip.choice_node
+local d = luasnip.dynamic_node
+local r = luasnip.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
+
+return {
+  snippet(
+    "test",
+    fmt(
+      [[
+        #[test]
+        def test [] {{
+          {}
+        }}
+            ]],
+      { i(0) }
+    )
+  ),
+}
