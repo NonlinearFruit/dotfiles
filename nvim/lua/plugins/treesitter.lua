da553eb8bdee93e552f4568c86b8b1ac0f8c3efc
NonlinearFruit <1123benji5813@gmail.com>
2024-02-03 10:43:13 -0700

    Enable tree sitter syntax highlighting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,30 @@
+local function configure()
+  require("nvim-treesitter.configs").setup({
+    -- A list of parser names, or "all" (the four listed parsers should always be installed)
+    ensure_installed = {
+      "c_sharp",
+      "lua",
+      "vim",
+      "python",
+      "jq",
+      "rust",
+      "yaml",
+      "toml",
+      "markdown",
+      "markdown_inline",
+      "latex",
+      "json",
+      "javascript",
+      "bash",
+    },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+  })
+end
+
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all" (the four listed parsers should always be installed)
-      ensure_installed = {
-        "c_sharp",
-        "lua",
-        "vim",
-        "python",
-        "jq",
-        "rust",
-        "yaml",
-        "toml",
-        "markdown",
-        "markdown_inline",
-        "latex",
-        "json",
-        "javascript",
-        "bash",
-      },
-    })
-  end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = configure,
 }
