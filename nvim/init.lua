230c73f3f1c75100e12759ec05c272dae87b2723
NonlinearFruit <1123benji5813@gmail.com>
2024-02-03 13:12:56 -0700

    Anti-n00b settings via psuedo lazy plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -66,23 +66,6 @@ local function setClipboard()
   }
 end
 
-local function setBacholerLife()
-  local function map(mode, key, cmd, description)
-    vim.keymap.set(mode, key, cmd, { desc = description })
-  end
-
-  -- Remove obnoxious bindings
-  map({ "n", "i" }, "<f1>", "<nop>", "This does nothing: Too close to Esc")
-
-  -- Remove newbie crutches!
-  map({ "n", "i", "v", "c" }, "<bs>", "<nop>", "This does nothing: Edit in normal mode")
-  map({ "n", "i", "v", "c" }, "<del>", "<nop>", "This does nothing: Edit in normal mode")
-  map({ "n", "i", "v", "c" }, "<down>", "<nop>", "This does nothing: Move in normal mode")
-  map({ "n", "i", "v", "c" }, "<left>", "<nop>", "This does nothing: Move in normal mode")
-  map({ "n", "i", "v", "c" }, "<right>", "<nop>", "This does nothing: Move in normal mode")
-  map({ "n", "i", "v", "c" }, "<up>", "<nop>", "This does nothing: Move in normal mode")
-end
-
 setBackups()
 setClipboard()
 setIndentation()
@@ -90,6 +73,3 @@ setLeader()
 setPlugins()
 tweakDisplay()
 tweakNetrw()
-if os.execute("is lonely") == 0 then
-  setBacholerLife()
-end
---
+++ nvim/lua/plugins/bacholer-life.lua
@@ -0,0 +1,25 @@
+local function configure()
+  local function map(mode, key, cmd, description)
+    vim.keymap.set(mode, key, cmd, { desc = description })
+  end
+
+  -- Remove obnoxious bindings
+  map({ "n", "i" }, "<f1>", "<nop>", "This does nothing: Too close to Esc")
+
+  -- Remove newbie crutches!
+  map({ "n", "i", "v", "c" }, "<bs>", "<nop>", "This does nothing: Edit in normal mode")
+  map({ "n", "i", "v", "c" }, "<del>", "<nop>", "This does nothing: Edit in normal mode")
+  map({ "n", "i", "v", "c" }, "<down>", "<nop>", "This does nothing: Move in normal mode")
+  map({ "n", "i", "v", "c" }, "<left>", "<nop>", "This does nothing: Move in normal mode")
+  map({ "n", "i", "v", "c" }, "<right>", "<nop>", "This does nothing: Move in normal mode")
+  map({ "n", "i", "v", "c" }, "<up>", "<nop>", "This does nothing: Move in normal mode")
+end
+
+return {
+  "local/bacholer-life",
+  cond = function()
+    return os.execute("is lonely") == 0
+  end,
+  config = configure,
+  dev = true,
+}
