f4977338ff1ec5d545b454a508f89e7fb60db5fa
NonlinearFruit <1123benji5813@gmail.com>
2024-02-09 07:34:43 -0700

    Use none-ls (#23)

---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,39 @@
+local function configure()
+  local null_ls = require("null-ls")
+
+  local INSTALL_THESE = {
+    -- Linters
+    -- { name = "selene", should_install = true },      -- Lua (GLIBC_2.32 not found)
+    { name = "golangci-lint", should_install = true }, -- Go
+    -- Formatters
+    { name = "stylua", should_install = os.execute("is termux") ~= 0 }, -- Lua
+    { name = "yamlfmt", should_install = true }, -- YAML
+  }
+  for _, pkg in ipairs(INSTALL_THESE) do
+    if pkg.should_install and not require("mason-registry").is_installed(pkg.name) then
+      require("mason.api.command").MasonInstall({ pkg.name })
+    end
+  end
+
+  local formatting = null_ls.builtins.formatting
+  local diagnostics = null_ls.builtins.diagnostics
+
+  null_ls.setup({
+    debug = false,
+    sources = {
+      formatting.stylua,
+      formatting.yamlfmt,
+      -- diagnostics.selene,
+      diagnostics.golangci_lint,
+    },
+  })
+end
+
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "williamboman/mason.nvim", -- Tool to install LSP/DAP/linter/formatters
+  },
+  config = configure,
+}
