78a3bfab4075ba39ed09d7878324745942ab6cf0
NonlinearFruit <1123benji5813@gmail.com>
2024-07-03 23:03:58 -0600

    Unify the typewriter prototype into single file

--- nvim/lua/plugins/typewriter.lua
+++ nvim/lua/plugins/typewriter.lua
@@ -1,5 +1,34 @@
+local api = vim.api
+local typewriter_active = false
+
+local function center_cursor()
+	if not typewriter_active then
+		return
+	end
+	api.nvim_command("normal! zzzszH")
+end
+
+local function enable_typewriter_mode()
+	if not typewriter_active then
+		typewriter_active = true
+    local group = vim.api.nvim_create_augroup("typewriter", { clear = true })
+    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+      group = group,
+      callback = center_cursor,
+    })
+	end
+end
+
+local function disable_typewriter_mode()
+	if typewriter_active then
+		typewriter_active = false
+		api.nvim_command("autocmd! typewriter")
+	end
+end
+
 local function configure()
-  require("prototypes.typewriter").setup()
+	api.nvim_create_user_command("TWEnable", enable_typewriter_mode, {})
+	api.nvim_create_user_command("TWDisable", disable_typewriter_mode, {})
 end
 
 return {
