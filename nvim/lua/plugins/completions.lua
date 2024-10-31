dbc9896abaa1fbb0fdebf2ec1ea51d19da3c98ab
NonlinearFruit <1123benji5813@gmail.com>
2024-10-31 07:05:10 -0600

    Move around in snippet completions

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,6 +1,7 @@
 local function configure()
   local cmp_ok, cmp = pcall(require, "cmp")
-  if not cmp_ok then
+  local luasnip_ok, luasnip = pcall(require, "luasnip")
+  if not cmp_ok or not luasnip_ok then
     return
   end
 
@@ -10,8 +11,34 @@ local function configure()
     mapping = cmp.mapping.preset.insert({
       ["<c-space>"] = cmp.mapping.complete(),
       ["<c-e>"] = cmp.mapping.abort(),
-      ["<tab>"] = cmp.mapping.confirm({ select = true }),
-      ["<cr>"] = cmp.mapping.confirm({ select = true }),
+      -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+      ["<cr>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          if luasnip.expandable() then
+            luasnip.expand()
+          else
+            cmp.confirm({ select = true })
+          end
+        else
+          fallback()
+        end
+      end),
+      ["<tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.confirm({ select = true })
+        elseif luasnip.locally_jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<s-tab>"] = cmp.mapping(function(fallback)
+        if luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
     }),
     sources = cmp.config.sources({
       { name = "nvim_lsp" },
