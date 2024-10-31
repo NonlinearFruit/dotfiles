fbaf84d0a8d0abc67423a1bb6cea27db62496bea
NonlinearFruit <1123benji5813@gmail.com>
2024-10-30 19:43:53 -0600

    Make long file names readable in telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -53,6 +53,13 @@ end
 
 return {
   "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      path_display = {
+        "smart"
+      }
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     {
