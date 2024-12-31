421a7d20710c6917595fc24d6410242dded9112f
NonlinearFruit <1123benji5813@gmail.com>
2024-12-31 13:55:54 -0600

    Pull bash lsp into bash plugin

--- nvim/lua/plugins/bash.lua
+++ nvim/lua/plugins/bash.lua
@@ -1,10 +1,37 @@
+local function install_lsp_and_dap_if_needed()
+  local INSTALL_THESE = {
+    "bash-language-server", -- LSP
+  }
+  for _, pkg in ipairs(INSTALL_THESE) do
+    if not require("mason-registry").is_installed(pkg) then
+      require("mason.api.command").MasonInstall({ pkg })
+    end
+  end
+end
+
+local function configure_lsp()
+  local lsp = require("language-server")
+  local lsp_config_ok, lsp_config = pcall(require, "lspconfig")
+  if not lsp_config_ok then
+    return
+  end
+
+  lsp_config.bashls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
+end
+
 local function configure()
-  vim.bo.filetype = "bash"
+  install_lsp_and_dap_if_needed()
+  configure_lsp()
+  vim.bo.filetype = "sh"
 end
 
 return {
   "local/bash",
   config = configure,
   event = "BufEnter /tmp/bash-fc*",
+  ft = "sh",
   virtual = true,
 }
