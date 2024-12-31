c3039b33df98a3b5966a34058ea93daee045bcea
NonlinearFruit <1123benji5813@gmail.com>
2024-12-31 13:41:06 -0600

    None-ls no longer needs mason

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,5 @@
 local function configure()
   local null_ls = require("null-ls")
-
-  local INSTALL_THESE = {
-    -- Linters
-    { name = "golangci-lint", should_install = true }, -- Go
-  }
-  for _, pkg in ipairs(INSTALL_THESE) do
-    if pkg.should_install and not require("mason-registry").is_installed(pkg.name) then
-      require("mason.api.command").MasonInstall({ pkg.name })
-    end
-  end
-
   local formatting = null_ls.builtins.formatting
   local diagnostics = null_ls.builtins.diagnostics
 
@@ -28,7 +17,6 @@ return {
   "nvimtools/none-ls.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "williamboman/mason.nvim", -- Tool to install LSP/DAP/linter/formatters
   },
   config = configure,
 }
