eaa12829528859cffc5870576dee1d15a724362b
NonlinearFruit <1123benji5813@gmail.com>
2024-12-31 14:02:37 -0600

    Pull graphql lsp into virtual plugin

---
+++ nvim/lua/plugins/graphql.lua
@@ -0,0 +1,35 @@
+local function install_lsp_and_dap_if_needed()
+  local INSTALL_THESE = {
+    "graphql-language-service-cli", -- LSP
+  }
+  for _, pkg in ipairs(INSTALL_THESE) do
+    if not require("mason-registry").is_installed(pkg) then
+      require("mason.api.command").MasonInstall({ pkg })
+    end
+  end
+end
+
+local function configure_lsp()
+  local lsp = require("language-server")
+  local lsp_config_ok, lsp_config = pcall(require, "lspconfig")
+  if not lsp_config_ok then
+    return
+  end
+
+  lsp_config.graphql.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
+end
+
+local function configure()
+  install_lsp_and_dap_if_needed()
+  configure_lsp()
+end
+
+return {
+  "local/graphql",
+  config = configure,
+  ft = "graphql",
+  virtual = true,
+}
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -37,11 +37,6 @@ local function configure()
   end
 
   if os.execute("is termux") ~= 0 then
-    lsp_config.graphql.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-
     lsp_config.openscad_lsp.setup({
       on_attach = on_attach,
       capabilities = capabilities,
