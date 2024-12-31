96f32719c5e1878cf5b44bac804df4cc7fadc0af
NonlinearFruit <1123benji5813@gmail.com>
2024-12-31 11:44:26 -0600

    Remove redundant lua ls setup

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -37,36 +37,6 @@ local function configure()
   end
 
   if os.execute("is termux") ~= 0 then
-    lsp_config.lua_ls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = "LuaJIT",
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = { "vim" },
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty = false,
-          },
-          -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = {
-            enable = false,
-          },
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-      },
-    })
-
     lsp_config.ts_ls.setup({
       on_attach = on_attach,
       capabilities = capabilities,
