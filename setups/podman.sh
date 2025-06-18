dd9f02d7ade3fb970274f004ce49f44b53f8d5fe
NonlinearFruit <1123benji5813@gmail.com>
2025-06-18 15:38:41 -0600

    Add standard dnf packages to the list

--- setups/podman.sh
+++
@@ -1,1 +0,0 @@
-sudo dnf install podman
--- setups/tools.sh
+++ setups/tools.sh
@@ -1,21 +1,3 @@
-# dos2unix: converts files from dos and unix and back again
-# figlet: ascii font
-# tmux: Terminal multiplexing
-# jq: json query
-# curl: http requests
-# unzip: extract zips (required by omnisharp)
-# xclip: copy/paste from clipboard
-# gcc-c++: c++ compiler (required by treesitter)
-# gcc: c compiler (required by rust)
-MANAGER=apt
-if command -v dnf > /dev/null; then
-  MANAGER=dnf
-fi
-sudo $MANAGER install -y curl figlet jq tmux unzip xclip gcc-c++ gcc g++
-
-# Load cargo into current shell
-source ~/.bashrc
-
 # speedtest-cli: collect up/down network data
 # pip install speedtest-cli
 # PATH="${PATH}:${HOME}/.local/bin"
