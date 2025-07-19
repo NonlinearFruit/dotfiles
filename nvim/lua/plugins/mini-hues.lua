ce52fea0d5fce98868b04f1ced399ec44dc9e362
NonlinearFruit <1123benji5813@gmail.com>
2025-07-18 22:37:42 -0600

    Make custom color scheme easier to reenable

--- nvim/lua/plugins/mini-hues.lua
+++ nvim/lua/plugins/mini-hues.lua
@@ -1,9 +1,17 @@
+local function set_color_scheme()
+  require('mini.hues').setup({
+    background = '#1c1c1c',
+    foreground = '#87afaf',
+    saturation = 'medium'
+  })
+end
+
+local function configure()
+  set_color_scheme()
+  vim.api.nvim_create_user_command("NonlinearFruitColorSchemeEnable", set_color_scheme, {})
+end
+
 return {
   "echasnovski/mini.hues",
-  version = "*",
-  opts = {
-    background = "#1c1c1c",
-    foreground = "#87afaf",
-    saturation = "medium",
-  },
+  config = configure,
 }
