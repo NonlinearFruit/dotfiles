8accaa56da2f9f885305501329268236bacc6c45
NonlinearFruit <1123benji5813@gmail.com>
2025-07-31 07:03:58 -0600

    Add golang lsp

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -1,11 +1,13 @@
 local function install_lsp_and_dap_if_needed()
   require("installer").install_if_missing({
+    "gopls",         -- LSP
     "golangci-lint", -- Linter
   })
 end
 
 local function configure()
   install_lsp_and_dap_if_needed()
+  vim.lsp.enable("gopls")
 end
 
 return {
