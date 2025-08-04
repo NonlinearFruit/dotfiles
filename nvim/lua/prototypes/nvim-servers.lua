accea3b86b7ae8866640f9d78fb414b55234dbec
NonlinearFruit <1123benji5813@gmail.com>
2025-08-04 15:51:36 -0600

    Send files between nvims

---
+++ nvim/lua/prototypes/nvim-servers.lua
@@ -0,0 +1,42 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+return {
+  send = function(opts, current_file)
+    opts = opts or {}
+    pickers
+      .new(opts, {
+        prompt_title = "nvim servers",
+        finder = finders.new_oneshot_job({ "nvim-list-servers" }, {
+          entry_maker = function(entry)
+            local current_server = vim.api.nvim_get_vvar("servername")
+            local server = vim.json.decode(entry)
+            if current_server == server.nvim_server_name then
+              return nil
+            end
+            return {
+              value = server,
+              display = server.tmux_window,
+              ordinal = server.tmux_window,
+            }
+          end,
+        }),
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            local server = selection.value
+            vim.fn.system("nvim --server " .. server.nvim_server_name .. " --remote " .. current_file)
+          end)
+          return true
+        end,
+      })
+      :find()
+  end,
+}
+-- to execute the function
+-- require("prototypes/nvim-servers").send(require("telescope.themes").get_dropdown{}, vim.api.nvim_buf_get_name(0))
