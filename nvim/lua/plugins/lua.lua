13349744e52ddb47192419878197291bebe57634
NonlinearFruit <1123benji5813@gmail.com>
2025-08-07 21:17:31 -0600

    Enable lua lsp

--- nvim/lua/plugins/lua.lua
+++ nvim/lua/plugins/lua.lua
@@ -1,27 +1,18 @@
 local function install_lsp_and_dap_if_needed()
   require("installer").install_if_missing({
+    "lua-language-server", -- LSP
     "stylua", -- Formatter
-  })
-end
-
-local function configure_lazydev()
-  require("lazydev").setup({
-    library = {
-      -- Load the wezterm types when the `wezterm` module is required
-      -- Needs `justinsgithub/wezterm-types` to be installed
-      { path = "wezterm-types", mods = { "wezterm" } },
-    },
   })
 end
 
 local function configure()
   install_lsp_and_dap_if_needed()
-  -- vim.lsp.enable("stylua")
-  configure_lazydev()
+  vim.lsp.enable("lua-language-server")
+  require("lazydev").setup()
 end
 
 return {
   "folke/lazydev.nvim",
-  ft = "lua", -- only load on lua files
+  ft = "lua",
   config = configure,
 }
