4d53f5411d9c97a86be607c990234bad6df336c2
NonlinearFruit <1123benji5813@gmail.com>
2025-08-07 22:32:57 -0600

    Add markdown lsp

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,10 +1,15 @@
 local function configure()
+  require("installer").install_if_missing({
+    "marksman", -- LSP requires `sudo dnf install libicu` or `DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1`
+  })
   vim.bo.filetype = "markdown"
+  vim.lsp.enable("marksman")
 end
 
 return {
   "local/markdown",
   config = configure,
+  ft = "markdown",
   event = "BufEnter *.page",
   virtual = true,
 }
