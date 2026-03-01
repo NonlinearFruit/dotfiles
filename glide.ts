f49d158662032b40088547d071fe89fd1a8ff3c0
NonlinearFruit <1123benji5813@gmail.com>
2026-03-01 06:50:00 -0700

    Steal config from @FarrukhKamal48 (https://github.com/FarrukhKamal48/.dotEndeavor)

--- glide.ts
+++ glide.ts
@@ -255,3 +255,56 @@ glide.keymaps.set("normal", ">>", async ({ tab_id }) => {
 
 glide.keymaps.set("normal", "u", "undo");
 glide.keymaps.set("normal", "<C-r>", "redo");
+
+glide.keymaps.set("normal", "gs", () => {
+    glide.hints.show({
+        selector: "div, section, article, aside, nav, ul, ol, pre, code, main, textarea, [class*='scroll']",
+
+        pick: async ({ content, hints }) => {
+            const results = await content.map((element: HTMLElement) => {
+                const style = window.getComputedStyle(element);
+
+                const overflowY = style.overflowY;
+                const overflowX = style.overflowX;
+                const isScrollableStyle =
+                    (overflowY === 'auto' || overflowY === 'scroll') ||
+                    (overflowX === 'auto' || overflowX === 'scroll');
+
+                const canScroll =
+                    element.scrollHeight > element.clientHeight ||
+                    element.scrollWidth > element.clientWidth;
+
+                const hasScrollClass = element.className.includes("scroll");
+                const isNotRoot = element !== document.body && element !== document.documentElement;
+
+                return (isScrollableStyle || hasScrollClass) && canScroll && isNotRoot;
+            });
+
+            const filteredHints = hints.filter((_, i) => results[i]);
+
+            if (filteredHints.length === 0) {
+                glide.excmds.execute("hints_remove");
+                glide.excmds.execute("mode_change normal");
+            }
+
+            return filteredHints;
+        },
+
+        action: async ({ content }) => {
+            await content.execute((element: HTMLElement) => {
+                element.scrollIntoView({ block: "center", behavior: "smooth" });
+
+                if (!element.hasAttribute("tabindex")) {
+                    element.setAttribute("tabindex", "-1");
+                }
+                element.focus();
+
+                const originalOutline = element.style.outline;
+                element.style.outline = "2px solid red";
+                setTimeout(() => {
+                    element.style.outline = originalOutline;
+                }, 600);
+            });
+        }
+    });
+}, { description: "Focus scrollable elements" });
