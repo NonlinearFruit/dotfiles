6aa2cf52ddff938e5bc357c906dc6be2c021a802
NonlinearFruit <1123benji5813@gmail.com>
2026-03-05 05:53:13 -0700

    Proper indent sizing

--- glide/gnarly-scrollable-picker.ts
+++ glide/gnarly-scrollable-picker.ts
@@ -1,46 +1,46 @@
 glide.keymaps.set("normal", "gs", () => {
-    glide.hints.show({
-        selector: "div, section, article, aside, nav, ul, ol, pre, code, main, textarea, [class*='scroll']",
+  glide.hints.show({
+    selector: "div, section, article, aside, nav, ul, ol, pre, code, main, textarea, [class*='scroll']",
 
-        pick: async ({ content, hints }) => {
-            const results = await content.map((element: HTMLElement) => {
-                const style = window.getComputedStyle(element);
+    pick: async ({ content, hints }) => {
+      const results = await content.map((element: HTMLElement) => {
+        const style = window.getComputedStyle(element);
 
-                const overflowY = style.overflowY;
-                const overflowX = style.overflowX;
-                const isScrollableStyle =
-                    (overflowY === 'auto' || overflowY === 'scroll') ||
-                    (overflowX === 'auto' || overflowX === 'scroll');
+        const overflowY = style.overflowY;
+        const overflowX = style.overflowX;
+        const isScrollableStyle =
+          (overflowY === 'auto' || overflowY === 'scroll') ||
+          (overflowX === 'auto' || overflowX === 'scroll');
 
-                const canScroll =
-                    element.scrollHeight > element.clientHeight ||
-                    element.scrollWidth > element.clientWidth;
+        const canScroll =
+          element.scrollHeight > element.clientHeight ||
+          element.scrollWidth > element.clientWidth;
 
-                const hasScrollClass = element.className.includes("scroll");
-                const isNotRoot = element !== document.body && element !== document.documentElement;
+        const hasScrollClass = element.className.includes("scroll");
+        const isNotRoot = element !== document.body && element !== document.documentElement;
 
-                return (isScrollableStyle || hasScrollClass) && canScroll && isNotRoot;
-            });
+        return (isScrollableStyle || hasScrollClass) && canScroll && isNotRoot;
+      });
 
-            const filteredHints = hints.filter((_, i) => results[i]);
+      const filteredHints = hints.filter((_, i) => results[i]);
 
-            if (filteredHints.length === 0) {
-                glide.excmds.execute("hints_remove");
-                glide.excmds.execute("mode_change normal");
-            }
+      if (filteredHints.length === 0) {
+        glide.excmds.execute("hints_remove");
+        glide.excmds.execute("mode_change normal");
+      }
 
-            return filteredHints;
-        },
+      return filteredHints;
+    },
 
-        action: async ({ content }) => {
-            await content.execute((element: HTMLElement) => {
-                element.scrollIntoView({ block: "center", behavior: "smooth" });
+    action: async ({ content }) => {
+      await content.execute((element: HTMLElement) => {
+        element.scrollIntoView({ block: "center", behavior: "smooth" });
 
-                if (!element.hasAttribute("tabindex")) {
-                    element.setAttribute("tabindex", "-1");
-                }
-                element.focus();
-            });
+        if (!element.hasAttribute("tabindex")) {
+          element.setAttribute("tabindex", "-1");
         }
-    });
+        element.focus();
+      });
+    }
+  });
 }, { description: "Focus scrollable elements" });
