266fd599603dad0139c3d5d9a2bc522a7cca1793
NonlinearFruit <1123benji5813@gmail.com>
2026-03-05 06:02:38 -0700

    Simplify keymap

--- glide/gnarly-scrollable-picker.ts
+++ glide/gnarly-scrollable-picker.ts
@@ -1,46 +1,50 @@
 glide.keymaps.set("normal", "gs", () => {
   glide.hints.show({
-    selector: "div, section, article, aside, nav, ul, ol, pre, code, main, textarea, [class*='scroll']",
-
-    pick: async ({ content, hints }) => {
-      const results = await content.map((element: HTMLElement) => {
-        const style = window.getComputedStyle(element);
-
-        const overflowY = style.overflowY;
-        const overflowX = style.overflowX;
-        const isScrollableStyle =
-          (overflowY === 'auto' || overflowY === 'scroll') ||
-          (overflowX === 'auto' || overflowX === 'scroll');
-
-        const canScroll =
-          element.scrollHeight > element.clientHeight ||
-          element.scrollWidth > element.clientWidth;
-
-        const hasScrollClass = element.className.includes("scroll");
-        const isNotRoot = element !== document.body && element !== document.documentElement;
-
-        return (isScrollableStyle || hasScrollClass) && canScroll && isNotRoot;
-      });
-
-      const filteredHints = hints.filter((_, i) => results[i]);
-
-      if (filteredHints.length === 0) {
-        glide.excmds.execute("hints_remove");
-        glide.excmds.execute("mode_change normal");
-      }
-
-      return filteredHints;
-    },
-
-    action: async ({ content }) => {
-      await content.execute((element: HTMLElement) => {
-        element.scrollIntoView({ block: "center", behavior: "smooth" });
-
-        if (!element.hasAttribute("tabindex")) {
-          element.setAttribute("tabindex", "-1");
-        }
-        element.focus();
-      });
-    }
+    selector: elements_that_could_scroll,
+    pick: pick_elements_that_are_overflowing,
+    action: scroll_to_element_and_focus
   });
-}, { description: "Focus scrollable elements" });
+}, { description: "[g]obsmacked find [s]crollable elements" });
+
+const elements_that_could_scroll =  "div, section, article, aside, nav, ul, ol, pre, code, main, textarea, [class*='scroll']"
+
+async function pick_elements_that_are_overflowing({ content, hints}) {
+  const results = await content.map((element: HTMLElement) => {
+    const style = window.getComputedStyle(element);
+
+    const overflowY = style.overflowY;
+    const overflowX = style.overflowX;
+    const isScrollableStyle =
+      (overflowY === 'auto' || overflowY === 'scroll') ||
+      (overflowX === 'auto' || overflowX === 'scroll');
+
+    const canScroll =
+      element.scrollHeight > element.clientHeight ||
+      element.scrollWidth > element.clientWidth;
+
+    const hasScrollClass = element.className.includes("scroll");
+    const isNotRoot = element !== document.body && element !== document.documentElement;
+
+    return (isScrollableStyle || hasScrollClass) && canScroll && isNotRoot;
+  });
+
+  const filteredHints = hints.filter((_, i) => results[i]);
+
+  if (filteredHints.length === 0) {
+    glide.excmds.execute("hints_remove");
+    glide.excmds.execute("mode_change normal");
+  }
+
+  return filteredHints;
+}
+
+async function scroll_to_element_and_focus({ content }) {
+  await content.execute((element: HTMLElement) => {
+    element.scrollIntoView({ block: "center", behavior: "smooth" });
+
+    if (!element.hasAttribute("tabindex")) {
+      element.setAttribute("tabindex", "-1");
+    }
+    element.focus();
+  });
+}
