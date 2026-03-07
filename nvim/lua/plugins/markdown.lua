ff2df938a4d3d1c8deda22b4f57bef27b816ee57
NonlinearFruit <1123benji5813@gmail.com>
2026-03-07 14:42:04 -0700

    All markdown files should have spell checking and autowrap

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -2,7 +2,9 @@ local function configure()
   require("installer").install_if_missing({
     "marksman", -- LSP requires `sudo dnf install libicu` or `DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1`
   })
-  vim.bo.filetype = "markdown"
+  vim.opt_local.filetype = "markdown"
+  vim.opt_local.spell = true
+  vim.opt_local.textwidth = 80
   vim.lsp.enable("marksman")
 end
 
