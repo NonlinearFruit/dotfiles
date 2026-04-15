5b1e9a4d795d0e872bf09170721c05e0e5787de5
NonlinearFruit <1123benji5813@gmail.com>
2026-04-15 15:37:12 -0600

    Ignore package version when checking if package is installed

--- nvim/lua/installer.lua
+++ nvim/lua/installer.lua
@@ -1,6 +1,7 @@
 local function install_if_missing(things_to_install)
   for _, pkg in ipairs(things_to_install) do
-    if not require("mason-registry").is_installed(pkg) then
+    local name = pkg:gsub("@.*$", "")
+    if not require("mason-registry").is_installed(name) then
       require("mason.api.command").MasonInstall({ pkg })
     end
   end
