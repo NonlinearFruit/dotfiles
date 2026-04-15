647e5f0e698a47450c71c5525a2437702b68697b
NonlinearFruit <1123benji5813@gmail.com>
2026-04-15 15:10:51 -0600

    nvim v0.12.1 with necessary nvim-treesitter update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,14 @@
-local function configure()
-  require("nvim-treesitter.configs").setup({
-    auto_install = true,
-    highlight = {
-      enable = true,
-    },
-  })
-end
-
 -- Print directory with treesitter parsers
 -- :lua= require("nvim-treesitter.configs").get_parser_install_dir()
 
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
-  config = configure,
+  opts = {
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+  },
 }
