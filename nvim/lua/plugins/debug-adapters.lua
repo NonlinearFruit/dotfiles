496c614e53d9856e41010707c2b506fe489022f4
NonlinearFruit <1123benji5813@gmail.com>
2026-06-04 10:34:23 -0600

    Switch debugging ui's

--- nvim/lua/plugins/debug-adapters.lua
+++ nvim/lua/plugins/debug-adapters.lua
@@ -4,118 +4,26 @@ local function key_mappings()
   end
 
   keymap("<f3>", "lua require('dap').run_last()", "Run last")
-  keymap("<f4>", "lua require('dap').repl.open()", "Open REPL")
+  keymap("<f4>", "DapViewJump repl", "Open REPL")
   keymap("<f5>", "lua require('dap').continue()", "Start debugger/Continue")
   keymap("<f9>", "lua require('dap').toggle_breakpoint()", "Toggle breakpoint")
   keymap("<f10>", "lua require('dap').step_over()", "Step over")
   keymap("<f11>", "lua require('dap').step_into()", "Step into")
   keymap("<f12>", "lua require('dap').step_out()", "Step out")
-  -- :lua require('dapui').close() to close all dap-ui windows
-  -- :lua require('dapui').toggle()
+  -- :DapViewClose to close all dap-view windows
+  -- :DapViewToggle
 end
 
-local function configure_dap_ui()
-  local dap_ok, dap = pcall(require, "dap")
-  if not dap_ok then
-    return
-  end
-  local dapui_ok, dapui = pcall(require, "dapui")
-  if not dapui_ok then
+local function configure_dap_view()
+  local dv_ok, dv = pcall(require, "dap-view")
+  if not dv_ok then
     return
   end
 
-  dapui.setup({
-    force_buffers = true,
-    icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-    mappings = {
-      -- Use a table to apply multiple mappings
-      expand = { "<CR>", "<2-LeftMouse>" },
-      open = "o",
-      remove = "d",
-      edit = "e",
-      repl = "r",
-      toggle = "t",
-    },
-    -- Use this to override mappings for specific elements
-    element_mappings = {
-      -- Example:
-      -- stacks = {
-      --   open = "<CR>",
-      --   expand = "o",
-      -- }
-    },
-    -- Expand lines larger than the window
-    -- Requires >= 0.7
-    expand_lines = vim.fn.has("nvim-0.7") == 1,
-    -- Layouts define sections of the screen to place windows.
-    -- The position can be "left", "right", "top" or "bottom".
-    -- The size specifies the height/width depending on position. It can be an Int
-    -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-    -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-    -- Elements are the elements shown in the layout (in order).
-    -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-    layouts = {
-      {
-        elements = {
-          -- Elements can be strings or table with id and size keys.
-          { id = "scopes", size = 0.25 },
-          "breakpoints",
-          "stacks",
-          "watches",
-        },
-        size = 40, -- 40 columns
-        position = "left",
-      },
-      {
-        elements = {
-          "repl",
-          "console",
-        },
-        size = 0.25, -- 25% of total lines
-        position = "bottom",
-      },
-    },
-    controls = {
-      -- Requires Neovim nightly (or 0.8 when released)
-      enabled = true,
-      -- Display controls in this element
-      element = "repl",
-      icons = {
-        pause = "",
-        play = "",
-        step_into = "",
-        step_over = "",
-        step_out = "",
-        step_back = "",
-        run_last = "↻",
-        terminate = "",
-      },
-    },
-    floating = {
-      max_height = nil, -- These can be integers or a float between 0 and 1.
-      max_width = nil, -- Floats will be treated as percentage of your screen.
-      border = "single", -- Border style. Can be "single", "double" or "rounded"
-      mappings = {
-        close = { "q", "<Esc>" },
-      },
-    },
-    windows = { indent = 1 },
-    render = {
-      indent = 2,
-      max_type_length = nil, -- Can be integer or nil.
-      max_value_lines = 100, -- Can be integer or nil.
-    },
+  -- https://igorlfs.github.io/nvim-dap-view/configuration#Defaults
+  dv.setup({
+    auto_toggle = true,
   })
-
-  dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
-  end
-  dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
-  end
-  dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
-  end
 
   vim.fn.sign_define("DapBreakpoint", { text = "🛑" })
   vim.fn.sign_define("DapStopped", { text = "💥" })
@@ -124,18 +32,16 @@ end
 local function configure()
   -- Resources:
   -- https://github.com/mfussenegger/nvim-dap
-  -- https://github.com/rcarriga/nvim-dap-ui
-  -- https://aaronbos.dev/posts/debugging-csharp-neovim-nvim-dap
+  -- https://github.com/igorlfs/nvim-dap-view
   key_mappings()
-  configure_dap_ui()
+  configure_dap_view()
 end
 
 return {
-  "rcarriga/nvim-dap-ui",
+  "igorlfs/nvim-dap-view",
   config = configure,
   event = "VeryLazy",
   dependencies = {
     "mfussenegger/nvim-dap",
-    "nvim-neotest/nvim-nio",
   },
 }
