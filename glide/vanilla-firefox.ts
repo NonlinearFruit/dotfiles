56763070630711847620da165121cb567ea43a58
NonlinearFruit <1123benji5813@gmail.com>
2026-06-10 22:29:12 -0600

    Firefox hardening recommendations from The New Oil
    <https://thenewoil.org/en/guides/most-important/browser>

--- glide/vanilla-firefox.ts
+++ glide/vanilla-firefox.ts
@@ -2,11 +2,18 @@
 glide.prefs.set("browser.startup.page", 3); // Open previous windows and tabs
 glide.prefs.set("signon.rememberSignons", false);
 glide.prefs.set("extensions.formautofill.creditCards.enabled", false);
+glide.prefs.set("extensions.formautofill.addresses.enabled", false);
 glide.prefs.set("ui.systemUsesDarkTheme", 1);
 glide.prefs.set("browser.tabs.splitView.enabled", true);
 // Disable "Alt" key from toggling top menu
 glide.prefs.set("ui.key.menuAccessKeyFocuses", false)
 glide.prefs.set("media.videocontrols.picture-in-picture.video-toggle.enabled", true);
+
+glide.prefs.set("privacy.donottrackheader.enabled", true);
+glide.prefs.set("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+glide.prefs.set("dom.security.https_only_mode", true);
+glide.prefs.set("network.trr.mode", 3); // DNS over HTTPS
+glide.prefs.set("network.trr.uri", "https://mozilla.cloudflare-dns.com/dns-query");
 
 // https://addons.mozilla.org
 glide.addons.install(
