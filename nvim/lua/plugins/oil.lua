cac59c7b0d05d706fd4b85a301d6ba5e226be24c
NonlinearFruit <1123benji5813@gmail.com>
2026-07-05 22:10:59 -0600

    Binding to grep current oil directory

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,6 +2,13 @@ local function configure()
   require("oil").setup({
     keymaps = {
       ["~"] = false,
+      ["<leader>/g"] = {
+        callback = function()
+          local dir = require("oil").get_current_dir()
+          require("telescope.builtin").live_grep({ cwd = dir })
+        end,
+        desc = "[/] [g]rep current oil directory",
+      },
     },
   })
   vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Browse parent directory" })
