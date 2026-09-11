19c2f23ca049f68c891a9d25a9e75e09b7481bba
NonlinearFruit <1123benji5813@gmail.com>
2026-09-11 16:30:49 -0600

    Handle multiple mappings at once

--- map.sh
+++ map.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
-mapping_to_apply="mappings/$1.json"
 json_to_symlink='.[] | if .[2] == "cp" then "cp -r \(.[0]) \(.[1])" elif .[2] == "curl" then "curl \(.[0]) -so \(.[1])" else "ln -sf $(pwd)/\(.[0]) \(.[1])" end'
 
-if [ -f "$mapping_to_apply" ]; then
-  jq "$json_to_symlink" --raw-output $mapping_to_apply
-fi
+for name in "$@"; do
+  mapping_to_apply="mappings/${name}.json"
+  if [ -f "$mapping_to_apply" ]; then
+    jq "$json_to_symlink" --raw-output $mapping_to_apply
+  fi
+done
